68a6adeb67d2ab70442dc183282b8904ea505c41
vindard <17693119+vindard@users.noreply.github.com>
2022-12-05 08:45:17 -0400

    feat: add fetchBalances method

--- test/demo.ts
+++ test/demo.ts
@@ -94,7 +94,15 @@ const main = async () => {
 
     const btcPrice = await sos.getCurrentPrice()
     if (btcPrice instanceof Error) throw btcPrice
-    console.log("Current BTC price is:", btcPrice.usdPerBtc)
+    const { usdPerBtc } = btcPrice
+    console.log("Current BTC price is:", usdPerBtc)
+
+    const balances = await sos.fetchBalances()
+    if (balances instanceof Error) throw balances
+    const { satsBalance } = balances
+    const fiatBalance = (satsBalance / 100_000_000) * usdPerBtc
+    console.log("Current sats balance is:", satsBalance)
+    console.log("Current sats balance is:", fiatBalance)
   }
 
   // ==========
